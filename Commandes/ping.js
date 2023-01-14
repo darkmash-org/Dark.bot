d64969a4dd1474bf65bea422ba6c153edb39c209
Rayreth <contactrayreth@gmail.com>
2023-01-14 21:52:48 +0100

    Update ping command

--- Commandes/ping.js
+++ Commandes/ping.js
@@ -4,14 +4,10 @@ const Command = require("../Structure/Command");
 module.exports = new Command({
 
     name: "ping",
-    description: "Permet de connaitre le ping du bot",
-    utilisation: "",
-    permission: "",
-    category: "Information",
 	cooldown: 5,
 
     async run(bot, message, args, db){
 
-        message.reply({ content: `🏓 Pong ! \`${bot.ws.ping}ms\`` })
+        message.reply({ content: `🏓 Pong \`${bot.ws.ping}ms\` !` })
     }
 })
