838e2c5959be61dfcd50fad4bcfed569fb8f72a9
Rayreth <contactrayreth@gmail.com>
2023-02-13 14:52:40 +0100

    Add ephemeral options to reply

--- Commandes/adddeadline.js
+++ Commandes/adddeadline.js
@@ -14,18 +14,18 @@ module.exports = new Command({
     const description = message.options.getString("description");
   
     if(!date.match(/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/)){
-        return message.reply("❌ The date is not valid !");
+        return message.reply({ content: "❌ The date is not valid !", ephemeral: true });
     }
     if(!time.match(/^([01]?[0-9]|2[0-3]):[0-5][0-9]$/)){
-        return message.reply("❌ The time is not valid !");
+        return message.reply({ content: "❌ The time is not valid !", ephemeral: true });
     }
     db.query(`INSERT INTO deadline_reminder (discord_id, discord_name, name, date, time, description) VALUES ('${message.user.id}', '${message.user.username}', '${name}', '${date}', '${time}', '${description}')`, (err, result) => {
         if(err) throw err;
         if(err) {
-            message.reply("❌ An error has occurred while adding the deadline !");
+            return message.reply({ content: "❌ An error has occurred while adding the deadline !", ephemeral: true });
         }
         else{
-        message.reply("✅ The deadline has been added successfully !");
+            return message.reply({ content: "✅ The deadline has been added successfully !", ephemeral: true });
         }
     }
     )  
